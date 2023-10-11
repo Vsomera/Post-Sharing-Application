abda6afc47048a589f8ab982d3cf3d87e8701952
Vilmor Somera <vsomera77@outlook.com>
2023-10-10 21:32:52 -0700

    dockerized authentication service

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -1,19 +1,19 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from 'cors'
-import connectDB from "./config/database"
+import connectDB from "../config/database"
 dotenv.config()
 connectDB()
 
 const app = express()
-const port = process.env.AUTH_PORT
+const port = process.env.AUTH_PORT || 5050
 
 app.use(express.json())
 app.use(cors())
 
 // Rest API Routes
 
-app.use("/api/users", require("./routes/userRoutes"))
+app.use("/api/users", require("../routes/userRoutes"))
 
 
 app.listen(port, () => {
